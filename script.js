5c0c9f8b03121de9880a3d4881f3cb83cfd64261
Dmitry Shambarov <shambarov63@gmail.com>
2021-12-01 21:31:54 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+// Manipulate Arrays With shift()
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+let removedFromMyArray = myArray.shift();
+
+
+// Manipulate Arrays With unshift()
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+
+//Shopping List
+const myList = [];
+myList.push(["Hello", 1]);
+myList.push(["Hell", 2]);
+myList.push(["Hel", 3]);
+myList.push(["He", 4]);
+myList.push(["H", 5]);
