7a7063f558e0d6e944401e59898be29b46bbe7e6
Dmitry Shambarov <shambarov63@gmail.com>
2021-12-01 22:08:37 +0400

    +3 tasks

--- script.js
+++ script.js
@@ -45,3 +45,53 @@ function reusableFunction() {
     function timesFive(num) {
         return num * 5
         };
+
+
+        //Global Scope and Functions
+        // Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+};
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+
+//Local Scope and Functions
+function myLocalScope() {
+    // Only change code below this line
+  let myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
+
+//Global vs. Local Scope in Functions
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+return 'sweater';
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
