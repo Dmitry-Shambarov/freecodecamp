bf635090daa93301054077d4c8b095255ce86921
Dmitry Shambarov <shambarov63@gmail.com>
2021-12-02 21:58:54 +0400

    +3 tasks

--- script.js
+++ script.js
@@ -95,3 +95,52 @@ return 'sweater';
 
 myOutfit();
 
+
+//
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive () {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+
+//Assignment with a Returned Value
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+
+//Stand in Line
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item)
+    let newItem = arr.shift()
+    
+    return newItem;
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
