2f1cf8001234beb1c100f9b612ebf2a063c2e7e6
Dmitry Shambarov <shambarov63@gmail.com>
2021-12-02 22:06:34 +0400

    +2 tasks

--- script.js
+++ script.js
@@ -144,3 +144,27 @@ function nextInLine(arr, item) {
   console.log("Before: " + JSON.stringify(testArr));
   console.log(nextInLine(testArr, 6));
   console.log("After: " + JSON.stringify(testArr));
+
+  //Understanding Boolean Values
+  function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
+
+
+  //Use Conditional Logic with If Statements
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if(wasThatTrue) {
+    return 'Yes, that was true'
+  } else {
+    return 'No, that was false'
+  }
+  
+  
+    // Only change code above this line
+  
+  }
