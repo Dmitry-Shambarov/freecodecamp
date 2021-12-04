bdf68b93d9b5de674038a0fe5da8247e26cc30bd
Dmitry Shambarov <shambarov63@gmail.com>
2021-12-04 21:34:37 +0400

    +1 task

--- script.js
+++ script.js
@@ -201,3 +201,13 @@ function nextInLine(arr, item) {
   let myString = "Hello, World!";
 let myRegex = /W/;
 let result = myRegex.test(myString); // Change this line
+
+//Match Literal Strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+//Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
