47da328015670bd1a91c517c25ae55068bb031ff
Dmitry Shambarov <shambarov63@gmail.com>
2021-12-05 21:12:47 +0400

    +1 task

--- script.js
+++ script.js
@@ -211,3 +211,35 @@ let result = waldoRegex.test(waldoIsHiding);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
+//Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+ switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
